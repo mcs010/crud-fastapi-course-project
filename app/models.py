5fb13ab4ea65abd5c2656d97214d24126155f49f
mcs010 <math.cds10@gmail.com>
2022-07-13 17:31:00 -0300

    feat(base-model): add created_at column to base model

--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
 from email.policy import default
+from pydoc import text
 from xmlrpc.client import Boolean
 from .database import Base
-from sqlalchemy import Column, Integer, String, Boolean
-from sqlalchemy.sql.expression import null
+from sqlalchemy import Column, Integer, String, Boolean, true
+from sqlalchemy.sql.expression import null, text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
 
 class Post(Base):
     __tablename__ = "posts"
@@ -11,3 +13,4 @@ class Post(Base):
     title = Column(String, nullable = False)
     content = Column(String, nullable = False)
     published = Column(Boolean, server_default = "True", nullable = False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
